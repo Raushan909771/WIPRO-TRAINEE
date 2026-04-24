7d9d499456de92d5f45ecf3058abe627c86f7e2c
Raushan909771 <kumararaushan@gmail.com>
2026-04-24 09:27:03 +0530

    python codes

---
+++ python/maths operation.py
@@ -0,0 +1,12 @@
+radius = int(input('Enter radius : '))
+
+print('Area : ',areaofcricle(radius = radius))
+print('Perimeter : ',perimeterofcricle(radius = radius))
+
+s = int(input('Enter side of sq :'))
+print('Area of Square : ', areaofsquare(side = s))
+print('perimeter of square : ', perimeterofsquare(side = s))
+
+l = int(input('Enter length : '))
+b = int(input('Enter breadth : '))
+print('Area of rec :', areaofrectangle(l,b))
